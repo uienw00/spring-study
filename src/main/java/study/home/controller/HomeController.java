0ea78491981278b2ce562cf4f02a443b2019fe1e
uienw00 <uienw00@gmail.com>
2017-08-07 10:26:46 +0900

    Thymeleaf 설정

--- src/main/java/study/home/controller/HomeController.java
+++ src/main/java/study/home/controller/HomeController.java
@@ -1,16 +1,24 @@
 package study.home.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import study.retry.service.RetryService;
+
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(){
+    public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("list", Arrays.asList("aaa", "bbb"));
+        model.addAttribute("name", "World");
+        return "home";
     }
 }
